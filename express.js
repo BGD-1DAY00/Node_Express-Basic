a7456bac90b09f6a0b02e687e94f24df0b52ec2c
BGD-1DAY00 <91921824+BGD-1DAY00@users.noreply.github.com>
2021-12-17 10:54:44 -0600

    inital commit

--- express.js
+++ express.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000; 
 const path = require('path')
 
-
+app.set('view engine', 'ejs')
 app.get('/',(req, res)=>{
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.render('index')
 });
 
 
